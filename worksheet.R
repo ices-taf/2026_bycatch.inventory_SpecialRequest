aaa9f8a29c22e03e036ba8a01d5c5074a83bfc55
colin millar <colinpmillar@gmail.com>
2026-03-27 13:13:28 +0100

    worksheet (can delete later)

--- worksheet.R
+++ worksheet.R
@@ -2,3 +2,24 @@ library(TAF)
 
 taf.skeleton()
 
+# development cycle
+
+# create data entry in DATA.bib
+# first time
+draft.data(file = TRUE)
+
+# populate data folder
+taf.boot()
+
+
+
+
+# next time
+draft.data(file = FALSE)
+
+# to run each section:
+source.taf("data")
+
+
+
+
